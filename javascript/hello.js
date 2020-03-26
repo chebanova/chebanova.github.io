6d842544fd1e117b4bbc6a60c1ad6c8b7ac4ff34
katerina <katerina-snow@mail.ru>
2020-03-26 16:27:04 +0100

    arrays/objects

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,56 @@
 // alert("Hello World");
+
+// let message = "Hello World";
+// alert(message);
+// message = "Hallo Welt";
+// alert(message);
+
+const LINK_COLOR = "#ff0000";
+console.log("Link bitte in der Farbe ", LINK_COLOR);
+
+let highscore = 520233;
+console.log(highscore / 10);
+
+let firstname = "John";
+let lastname = 'Smith';
+console.log("Name: ", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+
+let template = 'Dein Highscore sind ${highscore} Punkte';
+console.log(template);
+
+let isOver18 = true;
+console.log(isOver18);
+
+let age = 19;
+console.log("ueber 18?", age > 18);
+
+let partipants = ["John", "Jane", "Max"];
+console.log(partipants);
+console.log("Eintraege im Array: ", partipants.length);
+console.log(partipants[2]);
+
+let gameHighscores = [2099, 3010, 3333, 5000];
+console.log(gameHighscores);
+
+let user = {
+    firstname: "John",
+    lastname: "Smith",
+    age: 25
+}
+
+console.log(user);
+console.log(user.firstname);
+user.highcore = 200;
+user["highscore ever"] = 400;
+console.log(user);
+
+let a = 2;
+let b = 4;
+console.log(a+b);
+console.log(b/(a-1));
+a++;
+console.log(a);
+
