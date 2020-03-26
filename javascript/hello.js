9a5c025aa7b6eba5b3b931e9d83ab0c553ef1fde
katerina <katerina-snow@mail.ru>
2020-03-26 17:47:42 +0100

    data-lat data-lng

--- javascript/hello.js
+++ javascript/hello.js
@@ -126,6 +126,11 @@ let indetedParas = document.querySelectorAll(".indent");
 console.log(indetedParas);
 indetedParas.innerHTML = "Test2";
 indetedParas.forEach((para, index) => {
+    console.log(`Data attribute ${para.dataset.lat}.`)
     para.innerHTML = `Absatz ${index}`;
-    // para.style.color = "yellow";
+   if ( index % 2 == 0) {
+       para.style.color = "red";
+   } else {
+       para.style.color = "blue";
+   }
 });
