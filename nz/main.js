35075eb5d89be369e9c1abf9b5ad83d19ef686c4
katerina <katerina-snow@mail.ru>
2020-04-02 01:47:13 +0200

    map added

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,17 @@
 //alert("Hallo Welt!")
-var mymap = L.map('map').setView([-44.5, 169.133333], 13);
+
+var map = document.getElementById('map');
+
+let latitude = map.dataset.lat;
+let longitude = map.dataset.lng;
+let title = map.dataset.title;
+
+var mymap = L.map('map').setView([latitude, longitude], 10);
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 17,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    maxZoom: 15,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
+
+L.marker([latitude, longitude]).addTo(mymap)
+    .bindPopup(title).openPopup();
