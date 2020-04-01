709081c2312b46053fa99dae8d51f70186b055c6
chebanova <61839485+chebanova@users.noreply.github.com>
2020-04-01 22:57:54 +0200

    Update main.js

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,11 @@
 //alert("Hallo Welt!")
+var mymap = L.map('mapid').setView([-44.5, 169.133333], 13);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 17,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
