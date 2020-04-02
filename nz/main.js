999d0bb01a7d1e8194803d31cadd256a342d2cf3
katerina <katerina-snow@mail.ru>
2020-04-02 16:08:33 +0200

    leaflet

--- nz/main.js
+++ nz/main.js
@@ -1,17 +1,19 @@
-//alert("Hallo Welt!")
+let mapdiv = document.querySelector("#map");
 
-let map = document.getElementById('map');
+let map = L.map("map", {
+    center: [
+        mapdiv.dataset.lat,
+        mapdiv.dataset.lng
+    ],
+    zoom: 17,
+    layers: [
+        L.tileLayer.provider("OpenTopoMap")
+    ]
+});
 
-let latitude = map.dataset.lat;
-let longitude = map.dataset.lng;
-let title = map.dataset.title;
+let mrk = L.marker([
+    mapdiv.dataset.lat,
+    mapdiv.dataset.lng
+]).addTo(map);
 
-var mymap = L.map('map').setView([latitude, longitude], 10);
-
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 15,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(mymap);
-
-L.marker([latitude, longitude]).addTo(mymap)
-    .bindPopup(title).openPopup();
+mrk.bindPopup(mapdiv.dataset.title).openPopup();
