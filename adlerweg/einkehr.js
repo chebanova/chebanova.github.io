2fce7106f9ab8fccf879bdda01790b30c8616c88
katerina <katerina-snow@mail.ru>
2020-05-06 21:56:12 +0200

    Einkehrmöglichkeiten hinzugefuegt

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,17 @@
+const EINKEHR = [
+    [Etappennummer(18), HotelSchlossFernstein, 47.34563, 10.81764],
+    [Etappennummer(18), LoreaHuette, 47.34651, 10.79125],
+    [Etappennummer(18), HintereTarrentonalm, 47.32794, 10.72056],
+    [Etappennummer(18), AnhalterHuette, 47.30128, 10.66784],
+    [Etappennummer(17), Grubigalm, 47.38479, 10.86268],
+    [Etappennummer(17), HotelSchlossFernstein, 47.34563, 10.81764],
+    [Etappennummer(17), LoreaHuette, 47.34651, 10.79125],
+    [Etappennummer(16), Gaistalalm, 47.36667, 11.05],
+    [Etappennummer(16), Tillfussalm, 47.38333, 11.03333],
+    [Etappennummer(16), EhrwalderAlm, 47.38333, 10.96667],
+    [Etappennummer(15), Solsteinhaus, 47.31667, 11.28333],
+    [Etappennummer(15), EppzirlerAlm, 47.331042, 11.267198],
+    [Etappennummer(13), Patscherkofelhaus, 47.21048, 11.45230],
+    [Etappennummer(13), Boschebenalm, 47.21168, 11.47662],
+    [Etappennummer(13), Tulfeinalm, 47.22317, 11.52761],
+];
